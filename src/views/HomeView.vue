ee1e3a85e364168808d89bf8fdd138bbffc51ef9
naturallzh <naturallzh@163.com>
2024-04-15 23:52:31 +0800

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="test">
+    <div class="test">123</div>
+    <div class="inner">123</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.test{
+  color: blue;
+  .inner {
+    color: red;
+  }
+}
+</style>
