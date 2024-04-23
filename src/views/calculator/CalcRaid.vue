5e0424b2e9a6e02bd65637bbc985318373cc23c7
naturallzh <naturallzh@163.com>
2024-04-23 22:15:41 +0800

    Update CalcRaid.vue

--- src/views/calculator/CalcRaid.vue
+++ src/views/calculator/CalcRaid.vue
@@ -22,6 +22,7 @@
             <el-button type="danger" class="ml-9">清空</el-button>
           </template>
         </el-popconfirm>
+        (<el-icon style="color: rgb(24, 180, 24)"><Select /></el-icon>表示需要此BUFF且团队提供, <el-icon style="color: red"><Close /></el-icon>表示需要此BUFF但团队不提供, - 表示不需要此BUFF)
       </div>
       <div class="buff-res-container">
         <div class="buff-list">
@@ -57,7 +58,7 @@
               <div v-else-if="buffItem.enabled"><el-icon style="color: rgb(24, 180, 24)"><Select /></el-icon></div>
               <div v-else-if="!buffItem.enabled"><el-icon style="color: red"><Close /></el-icon></div>
               <div v-else>?</div>
-              <el-popconfirm v-if="mem.conflict[buffIdx] && !mem.provide.includes(buffIdx)" :title="(mem.conflict[buffIdx]?.length ? `该buff与${buffList[mem.conflict[buffIdx][0]].name}冲突，` : '') + '是否开启?'" width="200px" @confirm="confirmOpen(mem, buffIdx)">
+              <el-popconfirm v-if="mem.conflict[buffIdx] && !mem.provide.includes(buffIdx)" :title="(mem.conflict[buffIdx]?.length ? `该buff与${(mem.conflict[buffIdx].map(i => {return buffList[i].name})).join('、')}冲突，` : '') + '是否开启?'" width="400px" @confirm="confirmOpen(mem, buffIdx)">
                 <template #reference>
                   <el-button type="success" v-show="mem.conflict[buffIdx] && !mem.provide.includes(buffIdx)" link>开启</el-button>
                 </template>
@@ -154,10 +155,10 @@ const confirmReset = () => {
 }
 
 const buffList = [
-  { name: '嗜血 / 英勇', desc: '' }, { name: '5%全属性', desc: '' }, { name: '3%全伤害', desc: '' }, { name: '5%暴击', desc: '元素萨德天赋元素之誓按描述只提供法术暴击' },
-  { name: '力量敏捷', desc: '' }, { name: '4%物理易伤', desc: '' }, { name: '攻强', desc: '近战20% 远程10%' }, { name: '12%破甲', desc: '' },
-  { name: '10%攻速', desc: '' }, { name: '30%流血伤害', desc: '' }, { name: '10%法强', desc: '法师和非元素德萨满只能提供6%' }, { name: '5%法术急速', desc: '' },
-  { name: '8%法术易伤', desc: '' }, { name: '5%法术易爆', desc: '' }, { name: '耐力', desc: '' }, { name: '护甲', desc: '' },
+  { name: '嗜血 / 英勇', desc: '兽王猎由熔岩犬提供' }, { name: '5%全属性', desc: '兽王猎由页岩蜘蛛提供' }, { name: '3%全伤害', desc: '' }, { name: '5%暴击', desc: '元素萨德天赋元素之誓按描述只提供法术暴击；猎人由狼提供' },
+  { name: '力量敏捷', desc: '猎人由猫提供' }, { name: '4%物理易伤', desc: '猎人由掠夺者提供' }, { name: '攻强', desc: '近战20% 远程10%' }, { name: '12%破甲', desc: '猎人由迅猛龙提供' },
+  { name: '10%攻速', desc: '' }, { name: '30%流血伤害', desc: '猎人由鬣狗提供' }, { name: '10%法强', desc: '法师和非元素德萨满只能提供6%' }, { name: '5%法术急速', desc: '' },
+  { name: '8%法术易伤', desc: '猎人由风蛇提供' }, { name: '5%法术易爆', desc: '' }, { name: '耐力', desc: '猎人由异种虫提供' }, { name: '护甲', desc: '' },
   { name: '-10%物理受伤', desc: '' }, { name: '团队回蓝', desc: '每分钟回复1%' }, { name: '5秒回蓝', desc: '' },
 ]
 const buffListRes = ref([])
@@ -165,34 +166,34 @@ const classList = [
   { id: 'warr-1', name: '武器战', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [5, 9], conflict: {4: [14], 14: [4], 7: []}, desc: '', count: 0 },
   { id: 'warr-2', name: '狂暴战', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [3], conflict: {4: [14], 14: [4], 7: []}, desc: '', count: 0 },
   { id: 'warr-3', name: '防战', role: 't', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [7], conflict: {4: [14], 14: [4]}, desc: '', count: 0 },
-  { id: 'prie-1', name: '神牧', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [14, 16], conflict: [], desc: '', count: 0 },
-  { id: 'prie-2', name: '戒牧', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [14, 16], conflict: [], desc: '', count: 0 },
-  { id: 'prie-3', name: '暗牧', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [11, 14], conflict: [], desc: '', count: 0 },
-  { id: 'prie-1', name: '奥法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 2, 10], conflict: [], desc: '全局法强为6%', count: 0 },
-  { id: 'prie-2', name: '火法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 10, 13], conflict: [], desc: '全局法强为6%', count: 0 },
-  { id: 'prie-3', name: '冰法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 10, 17], conflict: [], desc: '全局法强为6%', count: 0 },
-  { id: 'warl-1', name: '痛苦术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [12, 13, 18], conflict: [], desc: '', count: 0 },
-  { id: 'warl-2', name: '恶魔术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [10, 12, 13], conflict: [], desc: '', count: 0 },
-  { id: 'warl-3', name: '毁灭术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [12, 13, 14, 17], conflict: [], desc: '', count: 0 },
+  { id: 'prie-1', name: '神牧', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [14, 16], conflict: {}, desc: '', count: 0 },
+  { id: 'prie-2', name: '戒牧', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [14, 16], conflict: {}, desc: '', count: 0 },
+  { id: 'prie-3', name: '暗牧', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [11, 14], conflict: {}, desc: '', count: 0 },
+  { id: 'prie-1', name: '奥法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 2, 10], conflict: {}, desc: '全局法强为6%', count: 0 },
+  { id: 'prie-2', name: '火法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 10, 13], conflict: {}, desc: '全局法强为6%', count: 0 },
+  { id: 'prie-3', name: '冰法', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 10, 17], conflict: {}, desc: '全局法强为6%', count: 0 },
+  { id: 'warl-1', name: '痛苦术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [12, 13, 18], conflict: {}, desc: '', count: 0 },
+  { id: 'warl-2', name: '恶魔术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [10, 12, 13], conflict: {}, desc: '', count: 0 },
+  { id: 'warl-3', name: '毁灭术', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [12, 13, 14, 17], conflict: {}, desc: '', count: 0 },
   { id: 'ro-1', name: '刺杀贼', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,12,13,14,15,16], passive: [12], conflict: {7: []}, desc: '上破甲对输出影响较大', count: 0 },
   { id: 'ro-2', name: '战斗贼', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [5], conflict:  {7: []}, desc: '上破甲对输出影响较大', count: 0 },
   { id: 'ro-3', name: '敏锐贼', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [3], conflict:  {7: [], 9: []}, desc: '上破甲对输出影响较大', count: 0 },
   { id: 'pal-1', name: '奶骑', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [], conflict: {1: [6, 18], 6: [1], 18: [1], 15: []}, desc: '', count: 0 },
   { id: 'pal-2', name: '防骑', role: 't', require: [0,1,2,3,4,5,6,7,8,14,15,16,17,18], passive: [], conflict: {1: [6, 18], 6: [1], 18: [1], 15: []}, desc: '', count: 0 },
   { id: 'pal-3', name: '惩戒骑', role: 'd1', require: [0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18], passive: [2, 17], conflict: {1: [6, 18], 6: [1], 18: [1], 15: []}, desc: '', count: 0 },
-  { id: 'hunt-1', name: '兽王猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [2], conflict: [], desc: '', count: 0 },
-  { id: 'hunt-2', name: '射击猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [6], conflict: [], desc: '', count: 0 },
-  { id: 'hunt-3', name: '生存猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [8], conflict: [], desc: '', count: 0 },
-  { id: 'dru-1', name: '平衡德', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [1, 11, 12], conflict: [], desc: '', count: 0 },
-  { id: 'dru-2', name: '熊德', role: 't', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [1, 3, 7, 9], conflict: [], desc: '', count: 0 },
-  { id: 'dru-3', name: '猫德', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [1, 3, 7, 9], conflict: [], desc: '', count: 0 },
-  { id: 'dru-4', name: '奶德', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [1, 17], conflict: [], desc: '', count: 0 },
+  { id: 'hunt-1', name: '兽王猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [2], conflict: {0:[1,3,4,5,7,9,12,15], 1:[0,3,4,5,7,9,12,15],3:[0,1,4,5,7,9,12,15],4:[0,1,3,5,7,9,12,15],5:[0,1,3,4,7,9,12,15],7:[0,1,3,4,5,9,12,15],9:[0,1,3,4,5,7,12,15],12:[0,1,3,4,5,7,9,15],15:[0,1,3,4,5,7,9,12]}, desc: '', count: 0 },
+  { id: 'hunt-2', name: '射击猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [6], conflict: {3:[4,5,7,9,12,15],4:[3,5,7,9,12,15],5:[3,4,7,9,12,15],7:[3,4,5,9,12,15],9:[3,4,5,7,12,15],12:[3,4,5,7,9,15],15:[3,4,5,7,9,12]}, desc: '', count: 0 },
+  { id: 'hunt-3', name: '生存猎', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [8], conflict: {3:[4,5,7,9,12,15],4:[3,5,7,9,12,15],5:[3,4,7,9,12,15],7:[3,4,5,9,12,15],9:[3,4,5,7,12,15],12:[3,4,5,7,9,15],15:[3,4,5,7,9,12]}, desc: '', count: 0 },
+  { id: 'dru-1', name: '平衡德', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [1, 11, 12], conflict: {}, desc: '', count: 0 },
+  { id: 'dru-2', name: '熊德', role: 't', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [1, 3, 7, 9], conflict: {}, desc: '', count: 0 },
+  { id: 'dru-3', name: '猫德', role: 'd1', require: [0,1,2,3,4,5,6,7,8,9,14,15,16], passive: [1, 3, 7, 9], conflict: {}, desc: '', count: 0 },
+  { id: 'dru-4', name: '奶德', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [1, 17], conflict: {}, desc: '', count: 0 },
   { id: 'sha-1', name: '元素萨', role: 'd2', require: [0,1,2,3,10,11,12,13,14,15,16,17,18], passive: [0, 10, 18], conflict: {4: [15], 15: [4], 8: [11], 11: [8]}, desc: '', count: 0 },
   { id: 'sha-2', name: '增强萨', role: 'd1', require: [0,1,2,3,4,5,6,7,8,10,12,13,14,15,16,17,18], passive: [0, 6, 10, 18], conflict: {4: [15], 15: [4], 8: [11], 11: [8]}, desc: '', count: 0 },
   { id: 'sha-3', name: '奶萨', role: 'h', require: [0,1,2,3,10,11,14,15,16,17,18], passive: [0, 10, 16, 18], conflict: {4: [15], 15: [4], 8: [11], 11: [8]}, desc: '', count: 0 },
   { id: 'dk-1', name: '血DK', role: 't', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [4], conflict: {6: []}, desc: '血DK可能不点加攻强', count: 0 },
-  { id: 'dk-2', name: '冰DK', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [4, 5, 8], conflict: [], desc: '', count: 0 },
-  { id: 'dk-3', name: '邪DK', role: 'd1', require: [0,1,2,3,4,5,6,7,8,11,12,13,14,15,16], passive: [4,12], conflict: [], desc: '', count: 0 },
+  { id: 'dk-2', name: '冰DK', role: 'd1', require: [0,1,2,3,4,5,6,7,8,14,15,16], passive: [4, 5, 8], conflict: {}, desc: '', count: 0 },
+  { id: 'dk-3', name: '邪DK', role: 'd1', require: [0,1,2,3,4,5,6,7,8,11,12,13,14,15,16], passive: [4,12], conflict: {}, desc: '', count: 0 },
 ]
 
 </script>
