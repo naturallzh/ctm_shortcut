ad14ea9cb3c18a5f0d53c73853843fdd3452e7aa
naturallzh <naturallzh@163.com>
2024-06-24 23:00:46 +0800

    Update common.js

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,13 @@
 import { ElMessage } from 'element-plus'
 
 const copyToclipboard = (val) => {
-  navigator.clipboard.writeText(val)
+  const tempEl = document.createElement('input');
+  tempEl.value = val;
+  document.body.appendChild(tempEl);
+  tempEl.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempEl);
+  // navigator.clipboard.writeText(val)
   ElMessage({
     message: '复制成功 使用 ctrl+v 进行粘贴',
     type: 'success',
