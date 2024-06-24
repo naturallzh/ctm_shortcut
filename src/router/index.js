9e72cdd86d18f94013cb898a3c23a3e4973584e8
naturallzh <naturallzh@163.com>
2024-06-24 21:11:53 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ import KitMacroGenerator from '../views/kit/KitMacroGenerator.vue'
 
 const router = createRouter({
   // history: createWebHashHistory(import.meta.env.BASE_URL),
-  history: createWebHistory('/'),
+  history: createWebHistory('/ctm'),
   routes: [
     {
       path: '/',
